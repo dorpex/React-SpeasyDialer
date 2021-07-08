9c202a1d460b5a1de1a63883c88ade6c09882335
dorpex <59210241+dorpex@users.noreply.github.com>
2021-07-08 22:23:58 +0300

    fads

--- client2/src/@core/auth/jwt/jwtDefaultConfig.js
+++ client2/src/@core/auth/jwt/jwtDefaultConfig.js
@@ -1,5 +1,5 @@
 // ** Auth Endpoints
-let domain = location.host === 'localhost:3000' ? 'localhost:9000': `165.227.148.234:9000`
+const domain = location.host === 'localhost:3000' ? 'localhost:9000': `165.227.148.234:9000`
 export default {
   loginEndpoint:  `http://${domain}/users/login/submit` ,
   registerEndpoint: `http://${domain}/users/register/submit`,
