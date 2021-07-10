72376709f04ed7d96505ce505cae5051d81eb2e9
dorpex <59210241+dorpex@users.noreply.github.com>
2021-07-10 17:23:26 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -25,16 +25,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  console.log('%O', req.method);
-  next();
-});
+
 
 // my app use
 app.use(cors());
@@ -55,6 +52,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
