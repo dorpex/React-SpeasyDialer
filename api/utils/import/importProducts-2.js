50df6ed3be100001e4bff6c68fa03c1dbcab95aa
dorpex <59210241+dorpex@users.noreply.github.com>
2021-07-11 12:48:58 +0300

    dsa

--- api/utils/import/importProducts-2.js
+++ api/utils/import/importProducts-2.js
@@ -4,8 +4,10 @@ const data = fs.readFileSync('dataBaseJson/products.json');
 const Product = require('../../../models/product')
 let fullData = JSON.parse(data);
 // fullData = fullData.filter(user => user.name != '')
-
+let x = 0
 fullData.forEach(productData => {
+    x++
     console.log(productData);
     new Product({oldId : productData.id , name : productData.name , serialNumber : productData.serial_number}).save()
+    console.log(`${fullData.length} - ${x}`);
 });
