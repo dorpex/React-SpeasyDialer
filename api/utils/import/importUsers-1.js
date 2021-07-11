c154162728416caec1c8d27ea1c2ffb033bda027
dorpex <59210241+dorpex@users.noreply.github.com>
2021-07-11 12:46:49 +0300

    fdas

--- api/utils/import/importUsers-1.js
+++ api/utils/import/importUsers-1.js
@@ -4,21 +4,23 @@ const data = fs.readFileSync('dataBaseJson/agents.json');
 const User = require('../../models/user')
 let fullData = JSON.parse(data);
 fullData = fullData.filter(user => user.name != '')
-let x = 0
-fullData.forEach(userData => {
-    x++ 
-    new User({
-        oldId : userData.id,
-        name : userData.name,
-        email : userData.email,
-        password : '1234',
-        status : false,
-        rank : userData.rank == 1 ? 'נציג'
-        : userData.rank == 2 ? 'נציג'
-        : userData.rank == 3 ? 'משווק'
-        : userData.rank == 4 ? 'מנהל מוקד' 
-        : userData.rank == 5 ? 'מנהל' : undefined,
-        published : userData.published == 1 ? true : false
-    }).save()
-    console.log(`${fullData.length} - ${x}`);
-});
+// let x = 0
+// fullData.forEach(userData => {
+//     x++ 
+//     new User({
+//         oldId : userData.id,
+//         name : userData.name,
+//         email : userData.email,
+//         password : '1234',
+//         status : false,
+//         rank : userData.rank == 1 ? 'נציג'
+//         : userData.rank == 2 ? 'נציג'
+//         : userData.rank == 3 ? 'משווק'
+//         : userData.rank == 4 ? 'מנהל מוקד' 
+//         : userData.rank == 5 ? 'מנהל' : undefined,
+//         published : userData.published == 1 ? true : false
+//     }).save()
+//     console.log(`${fullData.length} - ${x}`);
+// });
+
+User.find({}).then(e => console.log(e))
