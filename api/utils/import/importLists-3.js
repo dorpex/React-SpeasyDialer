a89df636acd598ba120a5f0f5613e92c586fc9fc
dorpex <59210241+dorpex@users.noreply.github.com>
2021-07-11 12:54:44 +0300

    dsa

--- api/utils/import/importLists-3.js
+++ api/utils/import/importLists-3.js
@@ -7,23 +7,27 @@ let fullData = JSON.parse(data);
 // console.log(fullData);
 // fullData = fullData.filter(user => user.name != '')
 let x = 0
-fullData.forEach(async listData => {
-    x++
-    let productId = await Product.findOne({oldId : listData.product_id}).select('_id')
-    let type = listData.type === '1' ? 'normal' : listData.type === '2' ? 'sold' : listData.type === '3' ? 'hot list' : null  
-    let status = listData.status === '1' ? 'send' : 'dont send' 
-    if (productId) {
-        productId = productId._id
-    }
-    new List({
-        oldId : listData.id,
-        name : listData.name,
-        type : type,
-        crmId : listData.crm_id,
-        product : productId,
-        status : status
-    }).save()
-    console.log(`${fullData.length} - ${x}`);
+// fullData.forEach(async listData => {
+//     x++
+//     let productId = await Product.findOne({oldId : listData.product_id}).select('_id')
+//     let type = listData.type === '1' ? 'normal' : listData.type === '2' ? 'sold' : listData.type === '3' ? 'hot list' : null  
+//     let status = listData.status === '1' ? 'send' : 'dont send' 
+//     if (productId) {
+//         productId = productId._id
+//     }
+//     new List({
+//         oldId : listData.id,
+//         name : listData.name,
+//         type : type,
+//         crmId : listData.crm_id,
+//         product : productId,
+//         status : status
+//     }).save()
+//     console.log(`${fullData.length} - ${x}`);
 
-    // new List({old_id : listData.id , name : listData.name , type , status  , product : productId , crm_id : listData.crm_id }).save()
-});
+//     // new List({old_id : listData.id , name : listData.name , type , status  , product : productId , crm_id : listData.crm_id }).save()
+// });
+List.find({})
+.then(e => {
+    console.log(e);
+})
